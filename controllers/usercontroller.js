dcd2dd684a4d92d4dfaf4cc98d6b3ede828a41fb
Parker K Catalano <parkerkcatalano@Admins-MacBook-Pro.local>
2018-10-22 17:23:09 -0400

    Added email for User model and typed out a bare-bones UserController.

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,78 +1,56 @@
-let app = require('koa')();
-let router = require('koa-router')();
-const sequelize = require('../db');
-const User = sequelize.import('../models/user');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const User = require('../db').import('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-/* CREATE USER */
-router.post('/createuser', function (req, res) {
-    let username = req.body.user.username
-    let pass = req.body.user.password
-    
-    User.create({
-        username: username,
-        passwordhash: bcrypt.hashSync(pass, 10)
-    })
-    .then(
-        function createSuccess(user) {
-           let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'});
-            
-            res.json({
-                user: user,
-                message: "created",
-                sessionToken: token
+class UserService {
+    _createToken(user) {
+        return jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '24h' })
+    }
+
+    async userCreate(userObj) {
+        try {
+            const createdUser = await User.create({
+                name: userObj.name,
+                email: userObj.email,
+                password: bcrypt.hashSync(userObj.password, 10)
             })
-        },
-        function createError(err) {
-            res.send(500, err.message)
-        }
-    )
-});
-
-/* SIGN IN */
-router.post('/signin', function(req, res){
-    User.findOne( { where: { username: req.body.user.username } } ).then(
-        function(user) {
-            if (user) {
-                bcrypt.compare(req.body.user.password, user.passwordhash, function(err, matches) {
-                    if (matches) {
-                        let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
-                        
-                        res.json({
-                            user: user,
-                            message: "Successfully authenticated",
-                            sessionToken: token
-                        })
-                    } else {
-                        res.status(502).send({ error: "Username or Password is invalid."})
-                    }
-                })
-            } else {
-                res.status(500).send({ error: "You failed to authenticate." });
+            return {
+                createdUser,
+                token: this._createToken(createdUser)
+            };
+        } catch (e){
+            return {
+                error:true,
+                e: e.errors[0].message
             }
-        },
-        function(err) {
-            res.status(501).send({error: "Login failed. Please try again."})
         }
-    )
-});
+    }
 
-/* DELETE USER */
-router.delete('/delete/:id', (req, res) => {
-    let userId = req.params.id
-
-    User
-    .destroy({
-        where: { id: userId }
-    }).then(
-        function deleteSuccess(data) {
-            res.send('User Deleted')
-        },
-        function deleteError(err) {
-            res.send(500, err.message)
+    async userLogin(userObj) {
+        try {
+            const loggedInUser = await User.findOne({ where: { email: userObj.email } })
+            const isUser = await bcrypt.compare(userObj.password, loggedInUser.password)
+            if(isUser){
+                return {
+                    loggedInUser,
+                    error: false,
+                    token: this._createToken(loggedInUser)
+                }
+            } else {
+                return {
+                    error: true,
+                    errorMsg: "Wrong username or password"
+                }
+            }
+        } catch (e) {
+            return {
+                error: true,
+                errorMsg: "User doesn't exist"
+            }
         }
-    )
-})
+    }
+}
+
+module.exports = UserService
 
 
--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('user', {
+        email: DataTypes.STRING,
         name: DataTypes.STRING,
+        password: DataTypes.STRING,
         account_id: DataTypes.INTEGER
     })
 }
