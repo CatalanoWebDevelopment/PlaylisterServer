47ce6f6df36399df6170dff013baf38ec7ff823d
Parker K Catalano <parkerkcatalano@Admins-MBP.office.app-press.com>
2018-10-26 12:14:44 -0400

    Media Controller Finished

--- src/controllers/mediacontroller.ts
+++ src/controllers/mediacontroller.ts
@@ -1,21 +1,89 @@
 import { sequelize } from "../db";
+import { createContext } from "vm";
 
 const Media = sequelize.import("../models/media");
 
+interface ErrorWithStatus extends Error {
+	status?: number;
+	message: string;
+}
+
 class MediaService {
 	async mediaCreate(accountId, groupId, mediaObj) {
-		const createdMedia = await Media.create({
-			account_id: accountId,
-			group_id: groupId,
-			name: mediaObj.name,
-			url: mediaObj.url,
-			thumb_url: mediaObj.thumb_url,
-			available: mediaObj.available,
-			size: mediaObj.size,
-			length: mediaObj.length
+		try {
+			const createdMedia = await Media.create({
+				accountId: accountId,
+				groupId: groupId,
+				name: mediaObj.name,
+				url: mediaObj.url,
+				thumb_url: mediaObj.thumb_url,
+				available: mediaObj.available,
+				size: mediaObj.size,
+				length: mediaObj.length
+			});
+
+			return createdMedia;
+		} catch (e) {
+			return {
+				error: true,
+				e: e.errors[0].message
+			};
+		}
+	}
+
+	async mediaFind(mediaId) {
+		const foundMedia = await Media.findOne({
+			where: { id: mediaId }
 		});
 
-		return createdMedia;
+		if (!foundMedia) {
+			const e: ErrorWithStatus = new Error("Media Not Found");
+			e.status = 404;
+			return `${e.message}, Status: ${e.status}`;
+		}
+
+		return foundMedia;
+	}
+
+	async mediaFindAll(accountId) {
+		const foundMedia = await Media.findAll({
+			where: { accountId: accountId }
+		});
+
+		if (!foundMedia) {
+			const e: ErrorWithStatus = new Error("No Medias Within Account");
+			e.status = 404;
+			throw `${e.message}, Status: ${e.status}`;
+		}
+
+		return foundMedia;
+	}
+
+	async mediaDelete(mediaId) {
+		const foundMedia = await Media.findOne({
+			where: { id: mediaId }
+		});
+
+		if (!foundMedia) {
+			const e: ErrorWithStatus = new Error("Media Not Found");
+			e.status = 404;
+			return `${e.message}, Status: ${e.status}`;
+		} else {
+			await Media.destroy({
+				where: { id: mediaId }
+			});
+
+			return { success: true };
+		}
+	}
+
+	async mediaUpdate(mediaId, mediaObj) {
+		const updatedMedia = await Media.update(
+			{ mediaObj },
+			{ where: { id: mediaId } }
+		);
+
+		return updatedMedia;
 	}
 }
 
--- src/routes/mediaRoutes.ts
+++ src/routes/mediaRoutes.ts
@@ -1,17 +1,24 @@
 import Router from "koa-router";
 import { mediaController } from "../controllers/mediacontroller";
+import { loginRequired } from "../middleware/authentication";
 
 export const mediaRouter = new Router();
 
-mediaRouter.post("/:accountId/:groupId", async ctx => {
-	const result = await mediaController.mediaCreate(
-		ctx.params.accountId,
-		ctx.params.groupId,
-		ctx.request.body
-	);
+mediaRouter.post(
+	"account/:accountId/group/:groupId",
+	loginRequired,
+	async ctx => {
+		const result = await mediaController.mediaCreate(
+			ctx.params.accountId,
+			ctx.params.groupId,
+			ctx.request.body
+		);
 
-	ctx.body = {
-		success: true,
-		result
-	};
-});
+		ctx.assert(result, 404, "Object Required");
+
+		ctx.body = {
+			success: true,
+			result
+		};
+	}
+);
